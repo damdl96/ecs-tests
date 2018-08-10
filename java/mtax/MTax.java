31938d828c0751babf6e64fe6e7feed7290f6e55
Diego Moreno <diegodm96@hotmail.com>
2018-08-10 12:02:21 -0500

    Refactor: Clearer structure plus optimized errors msgs ^---------^------------------------------------------^ |	  | |	  +----------->Re arranged some parts of the code |		       and rewrote some of the error messages. | | +----------------->Type: Refactor.

--- java/mtax/MTax.java
+++ java/mtax/MTax.java
@@ -3,17 +3,17 @@ import java.util.HashMap;
 import java.util.List;
 
 public class MTax implements Constant {
-    
+
     public MTax(){
-        
+
     }
-    
+
     public static List<String> validate(List<X_Tax> xTaxList) {
-        
+
         List<String> errorList = new ArrayList<>();
-        
+
 //        List<String> taxCategoryList = MInfoTaxCategory.getTaxCategoryStringList();
-        
+
         if(xTaxList != null && xTaxList.size() > 0) {
             List<String> validIds = new ArrayList<>();
             int cont = 0;
@@ -36,13 +36,13 @@ public class MTax implements Constant {
 //                    if(tax.isTrasladado() && tax.getTaxAmount() == null ) {
 //                        errorList.add("El importe es obligatorio");
 //                    }
-//                } 
+//                }
 //                else {
 //                    if(tax.getTaxAmount() == null ) {
 //                        errorList.add("El importe es obligatorio");
 //                    }
 //                }
-                
+
                 if(!tax.isLocal()){
                     cont++;
                 }
@@ -51,10 +51,9 @@ public class MTax implements Constant {
                 errorList.add("Debe de incluir al menos una tasa no local");
             }
             if(validIds.size() > 0){
-                    
                     List<X_Tax> xt = TaxsByListId(validIds, false);
                     if(xt.size() != validIds.size()){
-                        errorList.add("Existen datos no guardados previamente");
+                        errorList.add("No Existen datos guardados previamente");
                     }else{
                         HashMap<String, X_Tax> map_taxs = new HashMap<String, X_Tax>();
                         for(X_Tax tax: xt){
@@ -63,18 +62,17 @@ public class MTax implements Constant {
                         for(int i = 0; i < xTaxList.size(); i++){
                             if(xTaxList.get(i).getId() != null){
                                 xTaxList.get(i).setCreated(
-                                        map_taxs.get(xTaxList.get(i).getId().toString())
-                                                .getCreated());
+                                        map_taxs.get(xTaxList.get(i).getId().toString()).getCreated());
                             }
                         }
                     }
             }
         }
-//        else {
-//            errorList.add("El documento no tiene tasas");
-//        }
-        
+        else {
+            errorList.add("El documento no tiene tasas");
+        }
+
         return errorList;
     }
-    
+
 }
